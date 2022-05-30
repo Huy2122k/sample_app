814764fafb9cf7bc36982323a394715b22aa645d
Huy2122k <wwwhuy212@gmail.com>
2022-05-30 16:45:12 +0700

    add test

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'static_page/home'
   get 'static_page/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'static_page/about'
   # Defines the root path route ("/")
   root "application#hello"
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -5,7 +5,10 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
     get static_page_home_url
     assert_response :success
   end
-
+  test "should get about" do
+    get static_page_about_url
+    assert_response :success
+  end
   test "should get help" do
     get static_page_help_url
     assert_response :success
