bf4c740cbeb655ff9b471727f7737ddc6a6da2c3
Huy2122k <wwwhuy212@gmail.com>
2022-06-06 16:25:32 +0700

    done chapter 12

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,18 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "localhost", :port =>"3000"}
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
 
