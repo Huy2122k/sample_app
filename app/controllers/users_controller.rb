67cd7bebd2c4f564ecf946fa8f8a1f535b489622
Huy2122k <wwwhuy212@gmail.com>
2022-06-07 18:01:16 +0700

    end 14.2.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,10 +46,18 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
-
-
-
-
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
--- db/seeds.rb
+++ db/seeds.rb
@@ -32,4 +32,10 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
-
+# Create following relationships.
+users = User.all
+user = users.first
+following = users[2..15]
+followers = users[3..20]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
